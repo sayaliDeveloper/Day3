b4c8750f17bf75566fb5f045252719fb10000c76
Uptech <uptech@trichemie.com>
2023-06-08 09:39:42 +0530

    Added: Java Program of Star Problem 2

---
+++ src/com/bridgelabZ/assignment/StarPattern2.java
@@ -0,0 +1,23 @@
+package com.bridgelabZ.assignment;
+/*
+ *
+ **
+ ***
+ ****
+ *****
+
+ */
+
+public class StarPattern2 {
+    public static void main(String[] args) {
+        int row = 5;
+        int col = 1;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+            col++;
+        }
+    }
+}
